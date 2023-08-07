38828beb3f8eee30c57483b0171f0cc5899c29c1
bxwqsbkl <670809380@qq.com>
2023-08-07 14:07:26 +0800

    优化渠道选取算法

--- middleware/distributor.go
+++ middleware/distributor.go
@@ -84,7 +84,7 @@ func Distribute() func(c *gin.Context) {
 					modelRequest.Model = "dall-e"
 				}
 			}
-			channel, err = model.CacheGetRandomSatisfiedChannel(userGroup, modelRequest.Model)
+			channel, err = model.NewCacheGetRandomSatisfiedChannel(userGroup, modelRequest.Model)
 			if err != nil {
 				message := fmt.Sprintf("当前分组 %s 下对于模型 %s 无可用渠道", userGroup, modelRequest.Model)
 				if channel != nil {
